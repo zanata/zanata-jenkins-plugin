a28b37afeb3f2cc9049f16b9878e175a6c38d142
Yu Shao <yu.shao.gm@gmail.com>
2016-12-18 20:00:48 -0500

    Update ZanataBuilder.java

--- src/main/java/org/jenkinsci/plugins/zanata/ZanataBuilder.java
+++ src/main/java/org/jenkinsci/plugins/zanata/ZanataBuilder.java
@@ -1,7 +1,7 @@
 /*
 The MIT License (MIT)
 
-Copyright (c) 2016 Yu Shao
+Copyright (c) 2016 Yu Shao yu.shao.gm@gmail.com
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
